52992a6c995af4d269dc56d4369ef5f4334c015a
Rahman Ahmed <rahmanrafa@hotmail.com>
2025-02-28 10:01:46 -0500

    Froze Enums

--- src/difficulty.js
+++ src/difficulty.js
@@ -1,20 +1,20 @@
-const Difficulties = {
+const Difficulties = Object.freeze ({
     EASY: 0,
     MEDIUM: 1,
     HARD: 2
-}
+});
 
-export const DiffTargets = {
+export const DiffTargets = Object.freeze({
     EASY: 6,
     MEDIUM: 12,
     HARD:14
 
-}
+});
 
-export const DisplayTargets = {
+export const DisplayTargets = Object.freeze ({
     EASY: 4,
     MEDIUM: 8,
     HARD: 10
-}
+});
 
 export default Difficulties;
--- src/gamestate.js
+++ src/gamestate.js
@@ -1,9 +1,9 @@
-const GameStatus = {
+const GameStatus = Object.freeze( {
     MAINMENU: 0,
     RUNNING: 1,
     GAMEOVER: 2,
     GAMEWON: 3
-}
+});
 
 
 export default GameStatus;
