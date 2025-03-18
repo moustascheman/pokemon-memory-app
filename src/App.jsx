a4dd9fdb411257e25b014e12c77751837499fb62
Rahman Ahmed <rahmanrafa@hotmail.com>
2025-03-18 16:18:33 -0400

    Removed missing imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import GameStatus from './gamestate.js'
 import MainMenu from './components/MainMenu.jsx'
-import GameOver from './components/GameOver.jsx'
-import GameWon from './components/GameWon.jsx'
 import Difficulties from './difficulty.js'
 import './App.css'
 
